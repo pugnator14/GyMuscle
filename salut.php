1e92139ea2beeb2f539e5c948ae049a65d204161
pugnator14 <murgocivlad@yahoo.com>
2018-08-19 21:03:27 +0300

    Bine ma

---
+++ salut.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 19.08.2018
+ * Time: 20:19
+ /
+ <!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
